e3030ede967a4444cf92a14568deb7a83502bd88
yukuai0011 <kuyu5570@uni.sydney.edu.au>
2024-10-12 02:57:44 +1100

    Refactor booking.py to remove hardcoded input prompts and add .gitignore file
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- booking.py
+++ booking.py
@@ -9,10 +9,6 @@ UNIKEY = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 TOPT_CODE = os.getenv("TOPT_CODE")
 
-UNIKEY = input("Enter your unikey: ")
-PASSWORD = input("Enter your password: ")
-TOPT_CODE = input("Enter your totp code: ")
-
 with sync_playwright() as p:
     for browser_type in [p.chromium]:
         browser = browser_type.launch(headless=False, CHANNEL="msedge-beta")
@@ -25,23 +21,23 @@ with sync_playwright() as p:
                 page.goto("https://sso.sydney.edu.au")
                 # click button based on Xpath
                 page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[1]/div[2]/span/input"
+                    "/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[1]/div[2]/span/input"
                 ).first.fill(UNIKEY)
                 page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[2]/div[2]/span/input"
+                    "/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[2]/div[2]/span/input"
                 ).first.fill(PASSWORD)
                 page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[3]/div/span/div/label"
+                    "/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[3]/div/span/div/label"
                 ).click()
                 page.wait_for_timeout(1000)
                 page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/div/div[1]/div[2]/div[2]/a"
+                    "/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/div/div[1]/div[2]/div[2]/a"
                 ).click()
                 page.wait_for_timeout(1000)
                 # calculate totp
                 totp = pyotp.TOTP(TOPT_CODE)
                 page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[4]/div/div[2]/span/input"
+                    "/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[4]/div/div[2]/span/input"
                 ).first.fill(totp.now())
 
                 no_error = True
