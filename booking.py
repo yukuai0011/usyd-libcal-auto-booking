b04fb50400f786aa80128b6b0aef6e83e25ec47e
yukuai0011 <12008259+yukuai0011@users.noreply.github.com>
2024-10-12 14:06:26 +1100

    fix: add debug print statements for UNIKEY in booking.py (#13)
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- booking.py
+++ booking.py
@@ -3,11 +3,14 @@ import os
 import pyotp
 from playwright.sync_api import sync_playwright
 
+print("123")
+
 UNIKEY = os.getenv("UNIKEY")
 UNI_PASSWORD = os.getenv("UNI_PASSWORD")
 UNI_TOPT_CODE = os.getenv("UNI_TOPT_CODE")
 
 print(UNIKEY)
+print("123")
 
 with sync_playwright() as p:
     for browser_type in [p.chromium]:
