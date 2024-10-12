0253accdda090300e6a0b59a392bcdff150b511b
yukuai0011 <kuyu5570@uni.sydney.edu.au>
2024-10-12 15:11:45 +1100

    fix: improve error handling in booking workflow for better user experience
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- booking.py
+++ booking.py
@@ -24,156 +24,148 @@ with sync_playwright() as p:
         browser = browser_type.launch()
         page = browser.new_page()
 
-        no_error = False
+        page.goto("https://sso.sydney.edu.au")
+        print("Navigated to sso.sydney.edu.au")
+        # click button based on Xpath
+        page.locator(
+            "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[1]/div[2]/span/input"
+        ).first.fill(unikey)
+        page.locator(
+            "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[2]/div[2]/span/input"
+        ).first.fill(uni_password)
 
-        while not no_error:
-            try:
-                page.goto("https://sso.sydney.edu.au")
-                print("Navigated to sso.sydney.edu.au")
-                # click button based on Xpath
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[1]/div[2]/span/input"
-                ).first.fill(unikey)
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[2]/div[2]/span/input"
-                ).first.fill(uni_password)
+        page.locator(
+            "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[3]/div/span/div/label"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[3]/div/span/div/label"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/input"
+        ).click()
+        page.wait_for_timeout(1000)
+        page.locator(
+            "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/div/div[1]/div[2]/div[2]/a"
+        ).click()
+        page.wait_for_timeout(1000)
+        # calculate totp
+        totp = pyotp.TOTP(uni_topt_code)
+        page.locator(
+            "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[4]/div/div[2]/span/input"
+        ).first.fill(totp.now())
+        page.locator(
+            "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/input"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/input"
-                ).click()
-                page.wait_for_timeout(1000)
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/div/div[1]/div[2]/div[2]/a"
-                ).click()
-                page.wait_for_timeout(1000)
-                # calculate totp
-                totp = pyotp.TOTP(uni_topt_code)
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[4]/div/div[2]/span/input"
-                ).first.fill(totp.now())
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/input"
-                ).click()
+        page.wait_for_timeout(5000)
 
-                page.wait_for_timeout(5000)
+        page.goto("https://usyd.libcal.com/spaces?lid=3330&gid=0&c=0")
+        print("Navigated to usyd.libcal.com/spaces?lid=3330&gid=0&c=0")
 
-                page.goto("https://usyd.libcal.com/spaces?lid=3330&gid=0&c=0")
-                print("Navigated to usyd.libcal.com/spaces?lid=3330&gid=0&c=0")
+        for _ in range(14):
+            page.locator(
+                "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
+            ).click()
 
-                for _ in range(14):
-                    page.locator(
-                        "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
-                    ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[10]/td/div/div[2]/div[23]/a/div/div/div"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[10]/td/div/div[2]/div[23]/a/div/div/div"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
+        ).select_option("Arts and Social Sciences")
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
-                ).select_option("Arts and Social Sciences")
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
-                ).click()
+        page.goto("https://usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
+        print("Navigated to usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
 
-                page.goto("https://usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
-                print("Navigated to usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
+        for _ in range(14):
+            page.locator(
+                "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
+            ).click()
 
-                for _ in range(14):
-                    page.locator(
-                        "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
-                    ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[10]/td/div/div[2]/div[27]/a/div/div/div"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[10]/td/div/div[2]/div[27]/a/div/div/div"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
+        ).select_option("Arts and Social Sciences")
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
-                ).select_option("Arts and Social Sciences")
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
-                ).click()
+        page.goto("https://usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
 
-                page.goto("https://usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
+        for _ in range(2):
+            page.locator(
+                "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
+            ).click()
 
-                for _ in range(2):
-                    page.locator(
-                        "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
-                    ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[28]/td/div/div[2]/div[21]/a/div/div/div"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[28]/td/div/div[2]/div[21]/a/div/div/div"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
+        ).select_option("Arts and Social Sciences")
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
-                ).select_option("Arts and Social Sciences")
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
-                ).click()
+        page.goto("https://usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
+        print("Navigated to usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
 
-                page.goto("https://usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
-                print("Navigated to usyd.libcal.com/spaces?lid=3331&gid=0&c=0")
+        for _ in range(2):
+            page.locator(
+                "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
+            ).click()
 
-                for _ in range(2):
-                    page.locator(
-                        "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
-                    ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[28]/td/div/div[2]/div[27]/a/div/div/div"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[28]/td/div/div[2]/div[27]/a/div/div/div"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label"
+        ).click()
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label"
-                ).click()
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
+        ).select_option("Arts and Social Sciences")
 
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
-                ).select_option("Arts and Social Sciences")
-
-                page.locator(
-                    "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
-                ).click()
-
-                no_error = True
-            except Exception as e:
-                pass
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
+        ).click()
 
         browser.close()
