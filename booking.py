6e138d5f6f3483ce011be44b348d783fd8033b6b
yukuai0011 <12008259+yukuai0011@users.noreply.github.com>
2024-10-12 19:29:31 +1100

    using bookings json to add tasks (#32)
    * refactor: update booking script to accept JSON file for seat bookings and remove unused parameters
    
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>
    
    * refactor: add book_seat function signature to booking.py for improved seat booking logic
    
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>
    
    * refactor: add booking.json for seat booking configurations and update book_seat function for improved logic
    
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>
    
    * refactor: update booking JSON file path and replace deprecated booking.json with bookings.json
    
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>
    
    * refactor: iterate over booking tasks to call book_seat function for each task
    
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>
    
    ---------
    
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- booking.py
+++ booking.py
@@ -1,7 +1,9 @@
 import argparse
+import json
 
 import pyotp
 from playwright.sync_api import sync_playwright
+from playwright.sync_api._generated import Page
 
 print("123")
 
@@ -10,25 +12,60 @@ parser.add_argument("--unikey", required=True, help="Your USYD Unikey")
 parser.add_argument("--password", required=True, help="Your USYD password")
 parser.add_argument("--totp", required=True, help="Your TOTP secret code")
 parser.add_argument(
-    "--num-days-from-now", required=True, help="Number of days from now to book"
+    "booking-seats-json-path",
+    required=True,
+    help="Path to the JSON file containing the booking seats",
 )
-parser.add_argument(
-    "--booking-page-url", required=True, help="Number of hours from now to book"
-)
-parser.add_argument(
-    "--seat-full-xpath", required=True, help="Xpath of the button to book a full seat"
-)
-
 args = parser.parse_args()
 
 unikey = args.unikey
 uni_password = args.password
 uni_topt_code = args.totp
-num_days_from_now = args.num_days_from_now
-booking_page_url = args.booking_page_url
-seat_full_xpath = args.seat_full_xpath
+booking_seats_json_path = args.booking_seats_json_path
+
+with open(booking_seats_json_path, "r") as f:
+    booking_seats = json.load(f)
+
 
 print("123")
+
+
+def book_seat(
+    page: Page,
+    booking_page_url: str,
+    seat_full_xpath: str,
+    num_days_from_now: int = 1,
+):
+    page.goto(booking_page_url)
+    print(f"Navigated to {booking_page_url}")
+
+    for _ in range(int(num_days_from_now)):
+        page.locator(
+            "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
+        ).click()
+
+    page.locator(f"xpath={seat_full_xpath}").click()
+
+    page.locator(
+        "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
+    ).click()
+
+    page.wait_for_timeout(1000)
+
+    page.locator(
+        "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label/input"
+    ).click()
+
+    page.locator(
+        "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
+    ).select_option("Arts and Social Sciences")
+
+    page.locator(
+        "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
+    ).click()
+
+    page.wait_for_timeout(1000)
+
 
 with sync_playwright() as p:
     for browser_type in [p.chromium]:
@@ -68,32 +105,12 @@ with sync_playwright() as p:
 
         page.wait_for_timeout(5000)
 
-        page.goto(booking_page_url)
-        print(f"Navigated to {booking_page_url}")
-
-        for _ in range(int(num_days_from_now)):
-            page.locator(
-                "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
-            ).click()
-
-        page.locator(f"xpath={seat_full_xpath}").click()
-
-        page.locator(
-            "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
-        ).click()
-
-        page.wait_for_timeout(1000)
-
-        page.locator(
-            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label/input"
-        ).click()
-
-        page.locator(
-            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
-        ).select_option("Arts and Social Sciences")
-
-        page.locator(
-            "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
-        ).click()
+        for task in booking_seats["tasks"]:
+            book_seat(
+                page,
+                task["booking_page_url"],
+                task["seat_full_xpath"],
+                task["num_days_from_now"],
+            )
 
         browser.close()
