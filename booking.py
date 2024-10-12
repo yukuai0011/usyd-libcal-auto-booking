34db1a2db8fff1eebc589da6237a79fc7b5867bf
yukuai0011 <kuyu5570@uni.sydney.edu.au>
2024-10-13 02:31:21 +1100

    fix: restore booking.py to f2d70d05cd630a48876b1d39031dded4e39e4219, and add return type of book_seat function
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- booking.py
+++ booking.py
@@ -3,9 +3,7 @@ import json
 
 import pyotp
 from playwright.sync_api import sync_playwright
-from playwright.sync_api._generated import Browser
-
-import multiprocessing.pool
+from playwright.sync_api._generated import Page
 
 print("123")
 
@@ -33,92 +31,86 @@ print("123")
 
 
 def book_seat(
-    browser: Browser,
+    page: Page,
     booking_page_url: str,
     seat_full_xpath: str,
     num_days_from_now: int = 1,
-):
-    booking_page = browser.new_page()
-    booking_page.goto(booking_page_url)
+) -> None:
+    page.goto(booking_page_url)
     print(f"Navigated to {booking_page_url}")
 
     for _ in range(int(num_days_from_now)):
-        booking_page.locator(
+        page.locator(
             "xpath=/html/body/div[2]/main/div/div/div/div[3]/div[1]/div[1]/div[1]/div/button[2]"
         ).click()
 
-    booking_page.locator(f"xpath={seat_full_xpath}").click()
+    page.locator(f"xpath={seat_full_xpath}").click()
 
-    booking_page.locator(
+    page.locator(
         "xpath=/html/body/div[2]/main/div/div/div/div[4]/form/fieldset/div[2]/button"
     ).click()
 
-    booking_page.wait_for_timeout(1000)
+    page.wait_for_timeout(1000)
 
-    booking_page.locator(
+    page.locator(
         "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[4]/fieldset/div/div[1]/label/input"
     ).click()
 
-    booking_page.locator(
+    page.locator(
         "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[5]/div/select"
     ).select_option("Arts and Social Sciences")
 
-    booking_page.locator(
+    page.locator(
         "xpath=/html/body/div[2]/main/div/div/div/div/div[2]/form/fieldset/div[6]/div/button"
     ).click()
 
-    booking_page.wait_for_timeout(1000)
+    page.wait_for_timeout(1000)
 
 
 with sync_playwright() as p:
     for browser_type in [p.chromium]:
-        browser: Browser = browser_type.launch()
-        sign_in_page = browser.new_page()
+        browser = browser_type.launch()
+        page = browser.new_page()
 
-        sign_in_page.goto("https://sso.sydney.edu.au")
+        page.goto("https://sso.sydney.edu.au")
         print("Navigated to sso.sydney.edu.au")
         # click button based on Xpath
-        sign_in_page.locator(
+        page.locator(
             "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[1]/div[2]/span/input"
         ).first.fill(unikey)
-        sign_in_page.locator(
+        page.locator(
             "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[2]/div[2]/span/input"
         ).first.fill(uni_password)
 
-        sign_in_page.locator(
+        page.locator(
             "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[3]/div[3]/div/span/div/label"
         ).click()
 
-        sign_in_page.locator(
+        page.locator(
             "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/input"
         ).click()
-        sign_in_page.wait_for_timeout(1000)
-        sign_in_page.locator(
+        page.wait_for_timeout(1000)
+        page.locator(
             "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/div/div[1]/div[2]/div[2]/a"
         ).click()
-        sign_in_page.wait_for_timeout(1000)
+        page.wait_for_timeout(1000)
         # calculate totp
         totp = pyotp.TOTP(uni_topt_secret)
-        sign_in_page.locator(
+        page.locator(
             "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[1]/div[4]/div/div[2]/span/input"
         ).first.fill(totp.now())
-        sign_in_page.locator(
+        page.locator(
             "xpath=/html/body/div[2]/main/div[2]/div/div/div[2]/form/div[2]/input"
         ).click()
 
-        sign_in_page.wait_for_timeout(5000)
+        page.wait_for_timeout(5000)
 
-        tasks: list[tuple[Browser, str, str, int]] = [
-            (
-                browser,
+        for task in booking_seats["tasks"]:
+            book_seat(
+                page,
                 task["booking_page_url"],
                 task["seat_full_xpath"],
-                int(task["num_days_from_now"]),
+                task["num_days_from_now"],
             )
-            for task in booking_seats["tasks"]
-        ]
-
-        with multiprocessing.pool.ThreadPool(processes=4) as pool:
-            pool.starmap_async(book_seat, tasks).get()
 
         browser.close()
